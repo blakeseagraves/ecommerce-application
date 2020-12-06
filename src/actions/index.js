30fa68284f4b609ccb7843b14fcb00890e85eea8
Blake Seagraves <blake.seagraves2@gmail.com>
2020-12-05 21:14:56 -0600

    added search bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,11 +9,21 @@ import {
     setPurchaseDetail
 } from './user';
 
+import {
+    fetchShopCategories,
+    fetchShopProducts,
+    filterProductsWithCategoryId
+} from './shop';
+
 export {
     setHeaderLinks,
     setNavbarLinks,
     changeNavbarActive,
 
     fetchUserPurchases,
-    setPurchaseDetail
+    setPurchaseDetail,
+
+    fetchShopCategories,
+    fetchShopProducts,
+    filterProductsWithCategoryId
 };
---
+++ src/components/shop/shop.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+import ShopSearchBar from './shopSearchBar';
+
+class Shop extends Component {
+
+    componentDidMount() {
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Login',
+                path: '/signin'
+            }
+        ]
+        this.props.setHeaderLinks(headerLinks);
+        this.props.fetchShopCategories();
+
+        // filter products with links
+        this.props.fetchShopProducts();
+    }
+
+    shouldComponentUpdate(nextProps) {
+        if(this.props != nextProps) {
+            this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
+        }
+        return true
+    }
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+    render() {
+
+        return (
+            <div className='shop'>
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+                <div className='shop__products'>
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <div key={product._id} className='shop-product'>
+                                    <div className='shop-product__title'>
+                                        {product.title}
+                                    </div>
+                                    <div className='shop-product__description'>
+                                        {product.description}
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                {/* shop cart button */}
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    const { categories, filteredProducts } = state.shop;
+    return {
+        categories,
+        filteredProducts
+    } 
+}
+
+Shop = connect(mapStateToProps, actions)(Shop);
+
+export default Shop;
