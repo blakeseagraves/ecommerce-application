c4038358a1ac1a2ca1b92dc4484764826830a3cd
Blake Seagraves <blake.seagraves2@gmail.com>
2020-12-06 20:56:57 -0600

    added order review component

--- src/components/shop/shopCart.js
+++ src/components/shop/shopCart.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import CartProduct from './cartProduct';
 
-function CartButton({className, icon}) {
-    return (
-        <div className={`${className} cart-button`}>
-            <i className={icon}/>        
-        </div>
-    )
-}
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+import CartButton from './cartButton';
+
+import history from '../../history';
 
 function CartContent({className, products}) {
     let count = products.length;
-    let productsJSX = products.map(product => <h1 key={product}>{product}</h1>);
+    let productsJSX = products.map(product => <CartProduct {...product} key={product._id}/>);
     return (
         <div className={`${className} cart-content`}>
             <div className='cart-content__title'>
@@ -28,7 +28,7 @@ function CartFooter({className, products}) {
     const price = 7.96;
     return (
         <div className={`${className} cart-footer`}>
-            <a className='cart-footer__checkout'>
+            <a onClick={() => history.push('/order/review')} className='cart-footer__checkout'>
                 Checkout
             </a>
             <div className='cart-footer__subtotal'>
@@ -42,15 +42,37 @@ function CartFooter({className, products}) {
 }
 
 class ShopCart extends Component {
+
+    componentDidMount() {
+        this.props.fetchCartProducts();
+    }
+
+    handleAddToCart = () => {
+        if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
+            document.getElementById('shop-cart').classList.remove('cart-hidden');
+        } else {
+            document.getElementById('shop-cart').classList.add('cart-hidden');
+        }
+    }
+
     render() {
         const { className } = this.props;
         return (
-            <div className={`${className} shop-cart`}>
-                <CartButton className='shop-cart__toggle' icon='fas fa-times'/>
-                <CartContent className='shop-cart__content' products={[243, 3434, 4554]}/>
+            <div id='shop-cart' className={`${className} shop-cart cart-hidden`}>
+                <CartButton className='shop-cart__toggle' icon='fas fa-times' onClick={this.handleAddToCart}/>
+                <CartContent className='shop-cart__content' products={this.props.cartProducts}/>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    const { cartProducts } = state.user;
+    return {
+        cartProducts
+    }
+}
+
+ShopCart = connect(mapStateToProps, actions)(ShopCart);
 
 export default ShopCart;
